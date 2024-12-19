ca5a5f77b6a40dc546a5db1d6714dbc77708ef4d
Ghaiath <gm.trabulsi@gmail.com>
2024-12-19 22:19:25 +0100

    Post

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <PostList />
+  <CreatePost />
+</template>
+
+<script setup>
+import PostList from './components/PostList.vue';
+import CreatePost from './components/CreatePost.vue';
+</script>
