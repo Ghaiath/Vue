938a1c297e242b29df30626216fd70e62a2f0ba3
Ghaiath <gm.trabulsi@gmail.com>
2024-12-19 22:47:51 +0100

    Add Get

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <PostList />
-  <CreatePost />
 </template>
 
 <script setup>
 import PostList from './components/PostList.vue';
-import CreatePost from './components/CreatePost.vue';
 </script>
